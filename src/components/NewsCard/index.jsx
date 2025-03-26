85417dfa00fb203c71dcc2fe12573d1c64708f94
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2025-03-26 21:52:47 +0530

    News App: Style updates

--- src/components/NewsCard/index.jsx
+++ src/components/NewsCard/index.jsx
@@ -2,6 +2,8 @@ import './index.css'
 import Card from 'react-bootstrap/Card';
 
 function NewsCard({ article }) {
+  const publishedAt = new Date(article.publishedAt).toLocaleDateString("en-US", { weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' })
+
   return (
     <Card className='news-card'>
       <Card.Header
@@ -9,21 +11,31 @@ function NewsCard({ article }) {
       >
         <span>{article.source.name}</span>
         <span className='text-muted published-at'>
-          {new Date(article.publishedAt).toLocaleString()}
+          {publishedAt}
         </span>   
       </Card.Header>
       <Card.Img 
         variant="top" 
-        src={article.urlToImage} />
+        src={article.urlToImage}
+        className='news-img'
+      />
       <Card.Body>
-        <Card.Title>
-          {article.title}
+        <Card.Title className='news-title'>
+          {
+            article.title.length <= 80
+              ? article.title
+              : `${article.title.slice(0, 80)}...`
+          }
         </Card.Title>
         <Card.Text 
-          className='text-muted'
+          className='text-muted news-content'
           style={{ textAlign: 'justify' }}
         >
-          {article.description}
+          {
+            article.description.length <= 150
+              ? article.description
+              : `${article.description.slice(0, 150)}...`
+          }
         </Card.Text>
         <a 
           variant="primary"
